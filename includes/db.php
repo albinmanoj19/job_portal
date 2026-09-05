80e521d1af4043a43afbeb9a593b7baf6db2a048
albinmanoj <albinmanojkl33@gmail.com>
2026-09-05 15:16:35 +0530

    Connect PHP app to Railway MySQL

--- includes/db.php
+++ includes/db.php
@@ -1,14 +1,22 @@
 <?php
 
-$host = "localhost";
-$username = "root";
-$password = "";
-$database = "job_portal";
+$host = getenv('MYSQLHOST');
+$port = getenv('MYSQLPORT') ?: '3306';
+$user = getenv('MYSQLUSER');
+$password = getenv('MYSQLPASSWORD');
+$database = getenv('MYSQLDATABASE');
 
-$conn = new mysqli($host, $username, $password, $database);
+$conn = new mysqli(
+    $host,
+    $user,
+    $password,
+    $database,
+    (int)$port
+);
 
 if ($conn->connect_error) {
     die("Database connection failed: " . $conn->connect_error);
 }
 
+$conn->set_charset("utf8mb4");
 ?>
